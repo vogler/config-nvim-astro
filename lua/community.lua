7bba6f0af03ba86a348fb0dbce40e887eb781329
Ralf Vogler <ralf.vogler@gmail.com>
2025-06-08 22:31:14 +0200

    use markdown-and-latex.markview-nvim

--- lua/community.lua
+++ lua/community.lua
@@ -24,8 +24,10 @@ return {
   { import = "astrocommunity.media.vim-wakatime" },
   -- { import = "astrocommunity.color.headlines-nvim" },
   -- { import = "astrocommunity.markdown-and-latex.glow-nvim" },
-  -- { import = "astrocommunity.markdown-and-latex.peek-nvim" },
+  -- { import = "astrocommunity.markdown-and-latex.peek-nvim" }, -- :PeekOpen: Error: module not found
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  -- { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
+  { import = "astrocommunity.markdown-and-latex.markview-nvim" },
   -- { import = "astrocommunity.completion.copilot-lua" },
   { import = "astrocommunity.file-explorer.oil-nvim" }, -- <Leader>O
   { import = "astrocommunity.bars-and-lines.vim-illuminate" },
