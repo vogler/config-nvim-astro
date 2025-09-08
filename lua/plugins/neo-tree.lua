78dfdfd72de4091aa5022b779a7ecb941715b0bb
Ralf Vogler <ralf.vogler@gmail.com>
2025-09-08 22:45:43 +0200

    neo-tree: Z = expand_all_nodes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,6 +6,11 @@ return {
       opts.filesystem.filtered_items = {
         hide_gitignored = false,
       }
+      opts.window.mappings = {
+        -- ["z"] = "close_all_nodes",
+        ["Z"] = "expand_all_nodes",
+        --["Z"] = "expand_all_subnodes",
+      }
       return opts
     end,
   },
