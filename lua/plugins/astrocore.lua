00d1f3d1c7da962ebd7cbc35af9b9dbc32e888ed
Ralf Vogler <ralf.vogler@gmail.com>
2025-12-30 00:27:24 +0100

    cleanup template comments

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -60,6 +60,12 @@ return {
       n = {
         -- second key is the lefthand side of the map
 
+        -- tables with just a `desc` key will be registered with which-key if it's installed; this is useful for naming menus
+        -- ["<Leader>b"] = { desc = "Buffers" },
+
+        -- setting a mapping to false will disable it
+        -- ["<C-S>"] = false,
+
         ["-"] = { function() require("neo-tree.command").execute({ action = "focus", position = "current" }) end, desc = "Open Neotree in current window (netrw style)" },
         -- navigate buffer tabs
         ["<Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
@@ -82,24 +88,6 @@ return {
         -- ["go"] = { "gx", desc = "Open filepath or URI under cursor" }
         -- https://www.reddit.com/r/neovim/comments/161lm0k/how_to_remap_gx/
         -- maybe use this: https://github.com/chrisgrieser/nvim-various-textobjs#smarter-gx
-
-    --
-    --     -- mappings seen under group name "Buffer"
-    --     ["<Leader>bd"] = {
-    --       function()
-    --         require("astroui.status.heirline").buffer_picker(
-    --           function(bufnr) require("astrocore.buffer").close(bufnr) end
-    --         )
-    --       end,
-    --       desc = "Close buffer from tabline",
-    --     },
-    --
-    --     -- tables with just a `desc` key will be registered with which-key if it's installed
-    --     -- this is useful for naming menus
-    --     -- ["<Leader>b"] = { desc = "Buffers" },
-    --
-    --     -- setting a mapping to false will disable it
-    --     -- ["<C-S>"] = false,
       },
       i = {
         -- missing readline keybindings in insert mode like in https://github.com/tpope/vim-rsi
